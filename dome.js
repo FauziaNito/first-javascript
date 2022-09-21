5829632d8ac2d788cfb0315501f166ea10506a8e
Fauzia Nakacwa <fauzianaava@gmail.com>
2022-09-21 14:15:30 +0300

    Started javaScript in html and created these three files

---
+++ dome.js
@@ -0,0 +1,5 @@
+document.body.style.backgroundColor = "fuchsia";
+
+const html = document.documentElement;
+console.log(html.parentNode);
+console.log(document.getElementsByTagName('ul').childNodes);
--- js_in_html.js
+++ js_in_html.js
@@ -1,16 +1,25 @@
 //to bring control and validation in html, we use js in html
+
 //html does not deal directly with the computer memory so it is not a bonafide programming language.
+
 //HTML--Hyper Text Markup Language
-//HTML --Is used to markup text that represents other text(Hyper)
+
+//HTML --Is used to markup text that represents other text(Hypertext)
+
 //HTML --Is a language that we use to make or section text on web pages.
+
 //HTML & CSS without javaScript is like a beauty with no brains
+
 // html,css & js is ->Frontend Design--Junior Frontend Developer(JFD)
+
 //html,css & js are intended for a browser
+
 //The basic version of html,css & Js are called vanilla html,css & Js
 //html = dhtml || xhtml || html5
 // css = sass || bootstrap 5, 4 
 //Js  Versions are called frameworks including = 
 //js = Angular || React || vue || jQuery
+
 //It is incumbent upon css or js to provide means by which to acess or manipulate html
 //Css, html, js in the web browser is what we call a client side. The browser is a client.
 // Nodejs, tomcat, Microsoft IIS, Apache, are server side technologies
@@ -19,4 +28,4 @@
 // Afull stack development involues all the three technologies above.This makes 3-tier achitecture.
 // --The three communicates with one anothwer using a media called the internet using the HTTP protocol.
 // Note API are requests that request the desginsted server to feed a webserver.
-// (Example is Betting asking for customer deatails from mobile money account or bank CCOUNT)
+// (Example is Betting asking for customer deatails from mobile money account or bank account)
